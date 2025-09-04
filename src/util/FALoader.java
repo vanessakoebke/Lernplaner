ebd497a7688e53baa30e7efd7a40e8dc9d77110f
Vanessa Köbke <vanessa.koebke@gmail.com>
2025-09-04 17:12:11 +0200

    Classpath für Release angepasst

--- src/util/FALoader.java
+++ src/util/FALoader.java
@@ -1,14 +1,13 @@
 package util;
 
 import java.awt.Font;
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.InputStream;
 
 public class FALoader {
     public static Font loadFontAwesome() {
-        try {
-            File fontFile = new File("src/resources/Font Awesome 7 Free-Solid-900.otf");
-            Font font = Font.createFont(Font.TRUETYPE_FONT, new FileInputStream(fontFile));
+        try (InputStream is = FALoader.class.getResourceAsStream("/Font Awesome 7 Free-Solid-900.otf")) {
+            if (is == null) throw new RuntimeException("Font nicht gefunden im Klassenpfad!");
+            Font font = Font.createFont(Font.TRUETYPE_FONT, is);
             return font.deriveFont(16f); // Größe anpassen
         } catch (Exception e) {
             e.printStackTrace();
